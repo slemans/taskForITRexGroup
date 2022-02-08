0ad76052ac1ae43dbf6e5507674e5e84c3dee0c6
Pavel <sleman@yandex.ru>
2022-02-08 14:55:27 +0300

    start app

---
+++ taskForITRexGroup/taskForITRexGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskForITRexGroup
+//
+//  Created by sleman on 8.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
